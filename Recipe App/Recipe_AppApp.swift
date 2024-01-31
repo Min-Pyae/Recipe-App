00dfc838406441ec08d43c30ff0179c4b01c8047
Min Pyae <minmp2002@gmail.com>
2024-01-31 12:55:39 +0000

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Chris Min on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
